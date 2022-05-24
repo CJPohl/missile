aace4075b70e24dd9e238ae8481a1bad6d4f0235
Christopher J. Pohl <cjpohldev@gmail.com>
2022-05-24 17:03:15 -0400

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import { Center, Heading } from '@chakra-ui/react';
+const Home = () => {
+  return (
+    <Center>
+      <Heading>Hello, World</Heading>
+    </Center>
+  );
+};
+
+export default Home;
